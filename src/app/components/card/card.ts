4e9573d4f8336c3785abffca8e9bc35892b90b6c
SergiMT <sergimartineztorresbcn@gmail.com>
2025-10-19 16:03:28 +0200

    cards peliculas

--- src/app/components/card/card.ts
+++ src/app/components/card/card.ts
@@ -1,11 +1,11 @@
-import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Pelicula } from '../../models/pelicula';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage],
+  imports: [CommonModule],
   templateUrl: './card.html',
   styleUrl: './card.scss'
 })
--- src/app/pages/home.component/home.component.ts
+++ src/app/pages/home.component/home.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+﻿import { Component, inject, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Card } from '../../components/card/card';
+import { Pelicula } from '../../models/pelicula';
 
 @Component({
   selector: 'app-home.component',
-  imports: [],
+  standalone: true,
+  imports: [Card],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  private readonly http = inject(HttpClient);
+  protected readonly peliculas = signal<Pelicula[]>([]);
+  protected readonly items = this.peliculas;
 
+  constructor() {
+    this.http.get<Pelicula[]>('data/peliculas.json').subscribe({
+      next: (peliculas) => this.peliculas.set(peliculas),
+      error: (error) => console.error('No se pudo cargar el listado de peliculas', error)
+    });
+  }
 }
